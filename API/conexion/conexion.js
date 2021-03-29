a4e9ae7bd3edd5e53bc30ca3addc807473b98978
Cardozo1w <oscar1w2e@gmail.com>
2021-03-29 17:47:28 -0600

    Levantando Servidor y conectando base de datos

---
+++ API/conexion/conexion.js
@@ -0,0 +1,7 @@
+const sequelize = require("sequelize");
+
+const sequelize = new sequelize("administracion", "root", "root", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
---
+++ API/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+express.json();
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hola Mundo");
+});
+
+app.listen(3100, () => {
+  console.log("Servidor corriendo");
+});
